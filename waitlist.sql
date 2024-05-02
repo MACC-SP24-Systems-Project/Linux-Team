1a3807cb2e4dc65be20e92f3a19b6c99b1597435
Brenden Hoffman <13664514+brendenhoffman@users.noreply.github.com>
2024-05-01 20:18:39 -0500

    Update waitlist.sql

--- waitlist.sql
+++ waitlist.sql
@@ -8,35 +8,40 @@ DROP TABLE IF EXISTS `wait_requests`;
 
 -- Create the 'student' table
 CREATE TABLE `student` (
-    `student_id` INT AUTO_INCREMENT PRIMARY KEY,
-    `first_name` VARCHAR(255),
-    `last_name` VARCHAR(255)
-);
+  `student_id` INT NOT NULL AUTO_INCREMENT,
+  `email_address` VARCHAR(64) NOT NULL,
+  PRIMARY KEY (`student_id`),
+  UNIQUE KEY `student_id_UNIQUE` (`student_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 
 -- Create the 'course' table
 CREATE TABLE `course` (
-    `course_code` VARCHAR(255),
-    `term` VARCHAR(255),
-    `title` VARCHAR(255),
-    `campus` VARCHAR(255),
-    `credits` VARCHAR(255),
-    `date` VARCHAR(255),
-    `day` VARCHAR(255),
-    `faculty` VARCHAR(255),
-    `room` VARCHAR(255),
-    `seats` VARCHAR(255),
-    `status` VARCHAR(255),
-    `time` VARCHAR(255),
-    PRIMARY KEY (`course_code`, `term`)
-);
+  `course_code` VARCHAR(45) NOT NULL,
+  `term` VARCHAR(10) NOT NULL,
+  `course_title` VARCHAR(45) NOT NULL,
+  `instructor` VARCHAR(45) NOT NULL,
+  `seats` VARCHAR(45) NOT NULL,
+  `schedule` VARCHAR(45) NOT NULL,
+  `location` VARCHAR(45) NOT NULL,
+  `credits` DECIMAL(2,0) NOT NULL,
+  PRIMARY KEY (`course_code`,`term`),
+  KEY `term_idx` (`term`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 
 -- Create the 'wait_requests' table
 CREATE TABLE `wait_requests` (
-    `wait_id` INT AUTO_INCREMENT PRIMARY KEY,
-    `student_id` INT,
-    `course_code` VARCHAR(255),
-    `term` VARCHAR(255),
-    `time` DATETIME,
-    FOREIGN KEY (`student_id`) REFERENCES `student`(`student_id`),
-    FOREIGN KEY (`course_code`, `term`) REFERENCES `course`(`course_code`, `term`)
-);
+  `wait_id` INT NOT NULL AUTO_INCREMENT,
+  `student_id` INT NOT NULL,
+  `course_code` VARCHAR(45) NOT NULL,
+  `term` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`wait_id`),
+  UNIQUE KEY `wait_id_UNIQUE` (`wait_id`),
+  UNIQUE KEY `student_idx` (`student_id`),
+  KEY `term_idx` (`term`),
+  KEY `course_code_fk_idx` (`course_code`),
+  CONSTRAINT `course_code_fk` FOREIGN KEY (`course_code`) REFERENCES `course_data` (`course_code`),
+  CONSTRAINT `student_id_fk` FOREIGN KEY (`student_id`) REFERENCES `student_data` (`student_id`),
+  CONSTRAINT `term_fk` FOREIGN KEY (`term`) REFERENCES `course_data` (`term`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
